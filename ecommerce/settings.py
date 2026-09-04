975e9d818faeafbc897d8aca370808ba69d52445
Jaydeep <jaydeep@gmail.com>
2026-09-04 23:36:48 +0530

    Disable debug in production

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xw3vtu7v!qxkmbhe14mmx1kjyrcw^*7elp-4n7_q@l=)hw9ro7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
